8258a2024b9815f14c3e1865358331131e161c3f
aactaestfabula <beyzag1199@gmail.com>
2025-07-30 21:43:41 -0400

    Add files via upload

--- hello.py
+++ hello.py
@@ -5,8 +5,8 @@ y=input("Write anything to start ")
 print(y)
 namee=input("Can you write your name?")
 print("Hello",namee)
-tahminet=int(input("Guess a number (between 1-10)"))
-if tahminet==5:
+guess=int(input("Guess a number (between 1-10)"))
+if guess==5:
     print("Correct guess")
     print("Game over, congratulations")
     False

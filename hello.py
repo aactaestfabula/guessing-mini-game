50b7534e35759d56b53021faa6e2f9277da67637
aactaestfabula <beyzag1199@gmail.com>
2025-07-30 21:33:20 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+x=("Welcome to the game")
+print(x)
+print("ONLY 1 TRY, BE CAREFUL!!")
+y=input("Write anything to start ")
+print(y)
+namee=input("Can you write your name?")
+print("Hello",namee)
+tahminet=int(input("Guess a number (between 1-10)"))
+if tahminet==5:
+    print("Correct guess")
+    print("Game over, congratulations")
+    False
+else:
+    print("Wrong guess, sorry") 
+print(" Good attempt")
